e8a55cc96fd01c8eca8692edb69ae1d56adc7450
KBhadraGIT <kushal.bhadra.git@gmail.com>
2023-04-14 14:52:19 +0530

    Creating files and folders using template.py

--- template.py
+++ template.py
@@ -6,51 +6,42 @@ logging.basicConfig(
     format='[ %(asctime)s || %(levelname)s || %(message)s ]'
 )
 
-while True:
-    PROJECT = input("Enter the name of the project: ")
-    if PROJECT != "":
-        break
-
-logging.info(f"Creating new project: {PROJECT}")
-
-logging.info("Creating list of files present in the project {PROJECT}")
 
 list_of_files = [
-    f"src/__init__.py",
-    f"src/components/__init__.py",
-    f"src/components/data_ingestion.py",
-    f"src/components/data_validation.py",
-    f"src/components/data_transformation.py",
-    f"src/components/model_trainer.py",
-    f"src/components/model_evaluation.py",
-    f"src/components/model_pusher.py",
-    f"src/entity/__init__.py",
-    f"src/entity/config_entity.py",
-    f"src/entity/artifact_entity.py",
-    f"src/pipeline/__init__.py",
-    f"src/logger.py",
-    f"src/exception.py",
-    f"src/utils.py",
-    f"requirements.txt",
-    f"setup.py"
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_validation.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/components/model_pusher.py",
+    "src/entity/__init__.py",
+    "src/entity/config_entity.py",
+    "src/entity/artifact_entity.py",
+    "src/pipeline/__init__.py",
+    "src/logger.py",
+    "src/exception.py",
+    "src/utils.py",
+    "requirements.txt",
+    "setup.py"
 ]
 
-logging.info("Creating files in respective directories for the project {PROJECT}")
 
-for file_path in list_of_files:
-    logging.info("Here replacing all the '/' by '\'.")
-    file_path = Path(file_path)
-    logging.info("Separting directory and file name.")
-    file_dir, file_name = os.path.split(file_path)
 
-    if file_dir != '':
-        os.makedirs(file_dir, exist_ok= True)
-        logging.info(f"Creating directory: {file_dir}")
+for filepath in list_of_files:
+    
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if (not os.path.exists(file_dir)) or (os.path.getsize(file_dir) == 0):
-        with open(file_path, 'w') as f:
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating a directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
             pass
-        logging.info(f"Created file: {file_name} in directory: {file_path} ")
+            logging.info(f"Creating file: {filepath}")
 
     else:
-        logging.info(f'File already present in the directory.') 
+        logging.info(f"File already present: {filename}")
