733efd0e338c3fd33d85d2e6d6eee41edc6899f1
KBhadraGIT <kushal.bhadra.git@gmail.com>
2023-04-15 13:30:26 +0530

    Adding comments and update in config_entity.py

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -8,6 +8,7 @@ from datetime import datetime
 FILE_NAME = "sensor.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
+TEST_SIZE = 0.2
 
 
 class TrainingPipelineConfig:
@@ -33,14 +34,24 @@ class TrainingPipelineConfig:
 class DataIngestionConfig:
 
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        
         try:
             self.database_name="aps"
             self.collection_name="sensor"
+            
+            #Using the TrainingPipelineConfig creating directory:  artifact/__timestamp__/data_ingestion
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
+            
+            #Creating directory {feature_store} containing {sensor.csv}
             self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            
+            #Creating directory {dataset} containig {train.csv} || {test.csv}
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
-            self.test_size = 0.2
+            
+            #Population count of test dataset
+            self.test_size = TEST_SIZE
+
         except Exception  as e:
             raise APSException(e,sys)     
 
