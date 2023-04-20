53c527ea49e7e9a1270a02c7e08916d4124c6d2b
KBhadraGIT <kushal.bhadra.git@gmail.com>
2023-04-20 11:22:39 +0530

    Artifact for model evaluation created

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -25,3 +25,9 @@ class ModelTrainerArtifact:
     f1_train_score: float
     acc_test_score: float
     f1_test_score: float
+
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    improved_accuracy: float
